52a55b464d8c36514b5c2085f16306764e16f239
Aditya Chamim Pratama <pratamaditya7@gmail.com>
2018-03-13 00:07:36 +0700

    update tugas 95%

--- 4 - ri/client_mahasiswa.py
+++ 4 - ri/client_mahasiswa.py
@@ -5,50 +5,40 @@ import json
 ip_server = "127.0.0.1"
 port_server = 7777
 
-def tambah_mahasiswa():
+def add_mahasiswa():
     #kirim req GET dengan URL "/mahasiswa"
     #inisiasi koneksi ke server
     conn = http.client.HTTPConnection(ip_server, port=port_server)
-    
     #definisi header
     header = {"Content-type" : "application/json"}
-
     #definisi body
     mahasiswa_baru = {'nim': 210, 'nama':'joni', 'prodi':'SI'}
-
     #kirim req POST /mahasiswa
     conn.request('POST', '/mahasiswa', body=json.dumps(mahasiswa_baru), headers=header)
-
     #dapatkan response
     responese = conn.getresponse().read()
     print(responese.decode('utf-8'))
 
-def ubah_mahasiswa():
+def update_mahasiswa():
     #kirim req GET dengan URL "/mahasiswa"
     #inisiasi koneksi ke server
     conn = http.client.HTTPConnection(ip_server, port=port_server)
-    
     #definisi header
     header = {"Content-type": "application/json"}
-
     #definisi body
-    mahasiswa_baru = {'nama': 'Ratu', 'prodi': 'TI'}
-
+    mahasiswa_baru = {'nim': 111, 'nama': 'Cillesen', 'prodi': 'PTI'}
     #kirim req POST /mahasiswa
     conn.request('PUT', '/mahasiswa/123', body=json.dumps(mahasiswa_baru), headers=header)
-
     #dapatkan response
     responese = conn.getresponse().read()
     print(responese.decode('utf-8'))
 
-def hapus_mahasiswa():
+def delete_mahasiswa():
     #kirim req GET dengan URL "/mahasiswa"
     #inisiasi koneksi ke server
     conn = http.client.HTTPConnection(ip_server, port=port_server)
-
     #kirim req POST /mahasiswa
-    conn.request('DELETE', '/mahasiswa/456')
-
+    conn.request('DELETE', '/mahasiswa/111')
     #dapatkan response
     responese = conn.getresponse().read()
     print(responese.decode('utf-8'))
@@ -57,10 +47,8 @@ def getAll_mahasiswa():
     #kirim req GET dengan URL "/mahasiswa"
     #inisiasi koneksi ke server
     conn = http.client.HTTPConnection(ip_server, port=port_server)
-
     #kirim req ke server
     conn.request('GET', '/mahasiswa')
-
     #baca responese
     response = conn.getresponse().read()
     print( response.decode('utf-8') )
@@ -69,17 +57,10 @@ def get_mahasiswa():
     #kirim req GET dengan URL "/mahasiswa"
     #inisiasi koneksi ke server
     conn = http.client.HTTPConnection(ip_server, port=port_server)
-
     #kirim req ke server
     conn.request('GET', '/mahasiswa/456')
-
     #baca responese
     response = conn.getresponse().read()
     print( response.decode('utf-8') )
 
-#tambah_mahasiswa()
-getAll_mahasiswa()
-ubah_mahasiswa()
-#hapus_mahasiswa()
-#tambah_mahasiswa()
 getAll_mahasiswa()
--- 4 - ri/server_mahasiswa.py
+++ 4 - ri/server_mahasiswa.py
@@ -1,70 +1,95 @@
-#import library flask
-from flask import Flask, request, jsonify
-import json
+#import library flask, request dan jsonify
+from flask import Flask, request, jsonify, abort
 
 #inisiasi app flask sebagai server
-app = Flask("Hello App")
+app = Flask(__name__)
 
 data_mahasiswa = [
     {
         "nim" : 123,
-        "nama" : "Andi",
+        "nama" : "Messi",
         "prodi" : "TIF"
     },
     {
         "nim" : 456,
-        "nama" : "Budi",
+        "nama" : "Iniesta",
         "prodi" : "TEKKOM"
     },
     {
         "nim" : 789,
-        "nama" : "Sari",
+        "nama" : "Pique",
         "prodi" : "SI"
     }
 ]
 
-#fungsi handle tambah mahasiswa
+#fungsi index method GET URL /
+@app.route('/', methods=['GET'])
+def index():
+    return "Hello, World!<br>I'm Aditya"
+
+#fungsi menambah mahasiswa method POST URL /mahasiswa
 @app.route('/mahasiswa', methods=['POST'])
 def add_mahasiswa():
-    #baca body req
-    nim = request.json['nim']
-    nama = request.json['nama']
-    prodi = request.json['prodi']
+    #jika req yang tidak sesuai response 400 (bad req)
+    if not request.json or not 'nim' in request.json:
+        abort(400)
+    #baca body req dan dimasukan ke var array/json
     mahasiswa_baru = {
-        'nama' : nama,
-        'nim' : nim,
-        'prodi' : prodi
+        'nim': request.json['nim'],
+        'nama': request.json['nama'],
+        'prodi': request.json['prodi']
     }
-    #tambahkan ke list mahasiswa
+    #tambahkan ke list data_mahasiswa
     data_mahasiswa.append(mahasiswa_baru)
-    return 'OK - add'
+    #mengembalikan value mahasiswa baru
+    return jsonify({'data mahasiswa baru': mahasiswa_baru}), 201
 
-#fungsi update mahasiswa
-@app.route('/mahasiswa/<nim>', methods=['PUT'])
+#fungsi update mahasiswa method PUT URL /mahasiswa/nim
+@app.route('/mahasiswa/<int:nim>', methods=['PUT'])
 def update_mahasiswa(nim):
     data = [data for data in data_mahasiswa if data['nim'] == nim]
-    data_mahasiswa[0]['nama'] = request.json.get('nama', data_mahasiswa[0]['nama'])
-    data_mahasiswa[0]['prodi'] = request.json.get('prodi', data_mahasiswa[0]['prodi'])
-    return 'OK - update'
+    #jika req yang tidak sesuai response 400 (bad req)
+    if len(data) == 0:
+        abort(404)
+    if not request.json:
+        abort(400)
+    if 'nim' in request.json and type(request.json['nim']) != int:
+        abort(400)
+    if 'nama' in request.json and type(request.json['nama']) is not str:
+        abort(400)
+    if 'prodi' in request.json and type(request.json['prodi']) is not str:
+        abort(400)
+    data[0]['nim'] = nim
+    data[0]['nama'] = request.json.get('nama', data[0]['nama'])
+    data[0]['prodi'] = request.json.get('prodi', data[0]['prodi'])
+    return jsonify({'response':'Success Update'})
 
-#fungsi delete mahasiswa
-@app.route('/mahasiswa/<nim>', methods=['DELETE'])
+#fungsi delete mahasiswa method DELETE URL /mahasiswa/nim
+@app.route('/mahasiswa/<int:nim>', methods=['DELETE'])
 def delete_mahasiswa(nim):
     data = [data for data in data_mahasiswa if data['nim'] == nim]
+    #jika req yang tidak sesuai response 400 (bad req)
+    if len(data) == 0:
+        abort(404)
     data_mahasiswa.remove(data[0])
-    return 'OK - delete'
+    return jsonify({'response':'Success Delete'})
 
-#mendefinisikan fungsi yang akan handle method GET dengan URL '/'
+#fungsi yang akan handle method GET dengan URL '/mahasiswa'
 @app.route('/mahasiswa', methods=['GET'])
 #kembalikan data seluruh mahasiswa
 def getAll_mahasiswa():
     return jsonify({'data_mahasiswa': data_mahasiswa})
 
 @app.route('/mahasiswa/<int:nim>', methods=['GET'])
-#kembalikan data mahasiswa tertentu
+#kembalikan data mahasiswa tertentu method GET URL /mahasiswa/nim
 def get_mahasiswa(nim):
     data = [data for data in data_mahasiswa if data['nim'] == nim]
+    #jika url yg dimasukan tidak ditemukan di server, response 404
+    if len(data) == 0:
+        abort(404)
     return jsonify({'data_mahasiswa': data[0]})
 
 #jalankan server flask
-app.run(port=7777)
+if __name__ == '__main__':
+    app.run(port=7777)
+    
