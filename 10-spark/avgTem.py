a974b1bff6597eb14b66994d6458b18e1e87312b
Aditya Chamim Pratama <pratamaditya7@gmail.com>
2018-05-19 16:56:43 +0700

    upload dataset

---
+++ 10-spark/avgTem.py
@@ -0,0 +1,25 @@
+from pyspark.sql import SparkSession, functions
+
+spark = SparkSession \
+    .builder \
+    .appName("Python Spark SQL basic example") \
+    .config("spark.some.config.option", "some-value") \
+    .getOrCreate()
+
+df = spark.read.json("file:///data-kuliah/dataset/iot_devices.json")
+# df.show()
+
+# df.printSchema()
+
+# df.select("cca3").show()
+
+dfFilter = df.filter(df['cca3'] == "TCA")
+dfSum = dfFilter.select(functions.sum(dfFilter.temp).alias('temp'))
+
+dfCount = df.groupBy("cca3").count()
+dfFilterCount = dfCount.filter(dfCount['cca3'] == "TCA").select('count')
+
+avg = dfSum['temp'] / dfFilterCount['count']
+avg.show()
+
+#avg.saveAsTextFile("file:///data-kuliah/dataset/hasil_avg")
---
+++ 10-spark/wordcount.py
@@ -0,0 +1,31 @@
+from pyspark import SparkContext
+
+# Inisiasi spark context
+sc = SparkContext.getOrCreate()
+
+# Inisiasi rdd dari file text
+rdd = sc.textFile("file:///data-kuliah/10-spark/ibubudi.txt")
+
+# Transform untuk split line
+def split_line(line):
+    return line.split(" ")
+rddWord = rdd.flatMap(split_line)
+x = rddWord.collect()
+print(x)
+
+# Beri angka 1 sebagai value
+def give_number(word):
+    return (word.lower(),1)
+rddNumber = rddWord.map(give_number)
+y = rddNumber.collect()
+print(y)
+
+# Hitung / jumlahkan value dengan key yang sama
+def hitung(x,y):
+    return x+y
+hasil = rddNumber.reduceByKey(hitung)
+z = hasil.collect()
+print(z)
+
+# Cetak output ke file
+hasil.saveAsTextFile("file:///data-kuliah/10-spark/hasil_wordcount")
